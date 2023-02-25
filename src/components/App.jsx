66c6a894147018ad285c8b4bb771fefd1821e1dc
SvitlanaHonchar <snidlty@gmail.com>
2023-02-25 22:07:19 +0200

    Step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactList from './ContactList/ContactList';
+import Phonebook from './Phonebook/Phonebook';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 30,
+          color: '#010101',
+        }}
+      >
+        <Phonebook />
+        <ContactList contacts={this.state.contacts} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class Phonebook extends Component {
+  state = {
+    name: '',
+  };
+
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    // console.log(e.target.value);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const contact = {
+      name: this.state.name,
+    };
+    console.log(contact);
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="">
+            <span>Name</span>
+            <br />
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              placeholder="name"
+              value={this.state.name}
+              onChange={this.handleInput}
+              required
+            />
+          </label>
+          <br />
+          <button type="submit">Add contact</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Phonebook;
